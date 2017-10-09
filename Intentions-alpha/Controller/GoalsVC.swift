2b7d8c9380a177341d91a3c2fee018d10b1a2cd1
Alex Villacres <Alex71197@yahoo.com>
2017-10-08 17:33:18 -0700

    Initial commit with UI - no Logic

---
+++ Intentions-alpha/Controller/GoalsVC.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intentions-alpha
+//
+//  Created by Alex Villacres on 10/8/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import UIKit
+
+class GoalsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Intentions-alpha/Controller/RegisterVC.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterVC.swift
+//  Intentions-alpha
+//
+//  Created by Alex Villacres on 10/8/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import UIKit
+
+class RegisterVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
