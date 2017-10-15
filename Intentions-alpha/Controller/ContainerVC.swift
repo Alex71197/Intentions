23402fbb20182282088f582fec010a3e6214178b
Alex Villacres <Alex71197@yahoo.com>
2017-10-14 19:06:14 -0700

    Added most of the functioanlity for the reveal feature, just need to animate.

---
+++ Intentions-alpha/Controller/ContainerVC.swift
@@ -0,0 +1,19 @@
+//
+//  ContainerVC.swift
+//  Intentions-alpha
+//
+//  Created by Alex Villacres on 10/14/17.
+//  Copyright © 2017 Alex Villacres. All rights reserved.
+//
+
+import UIKit
+
+class SideMenuContainerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- Intentions-alpha/Controller/GoalsVC.swift
+++ Intentions-alpha/Controller/GoalsVC.swift
@@ -11,11 +11,27 @@ import UIKit
 class GoalsVC: UIViewController {
     
     // Outlets
+    @IBOutlet weak var welcomeStack: UIStackView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var sideMenuConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sideMenu: UIView!
+    
+    // Variables
+    var sideMenuOpen = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.swipedLeft))
+        swipeLeft.direction = UISwipeGestureRecognizerDirection.left
+        self.view.addGestureRecognizer(swipeLeft)
+        
+        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.swipedLeft))
+        swipeRight.direction = UISwipeGestureRecognizerDirection.right
+        self.view.addGestureRecognizer(swipeRight)
+        
     }
 
     @IBAction func addGoalBtnPressed(_ sender: Any) {
@@ -24,7 +40,41 @@ class GoalsVC: UIViewController {
     }
     
     @IBAction func menuBtnPressed(_ sender: Any) {
+        UIView.animate(withDuration: 0.3) {
+            self.sideMenu.transform = CGAffineTransform(translationX: 0, y: 0)
+            self.updateConstraints()
+        }
         
+        
+    }
+    
+    @objc func swipedLeft() {
+        updateConstraints()
+    }
+    
+    
+    func setUpSideMenuView(_ shadowOn: Bool) {
+        if shadowOn {
+            sideMenu.layer.shadowOffset = CGSize.zero
+            sideMenu.layer.shadowColor = UIColor.black.cgColor
+            sideMenu.layer.shadowOpacity = 1
+            sideMenu.layer.shadowRadius = 10
+        } else {
+            sideMenu.layer.shadowColor = UIColor.clear.cgColor
+        }
+    }
+    
+    func updateConstraints() {
+        welcomeStack.isHidden = true
+        if sideMenuOpen {
+            setUpSideMenuView(false)
+            sideMenuConstraint.constant = -300
+            sideMenuOpen = false
+        } else {
+            setUpSideMenuView(true)
+            sideMenuConstraint.constant = 0
+            sideMenuOpen = true
+        }
     }
     
     
